ace981bbeed944c7de6de853c19de62c1c3dad5d
punit45 <ipunit96@gmail.com>
2022-07-04 09:31:21 +0530

    template deign half(no media queries)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { PageoneComponent } from './components/common/pageone/pageone.component';
+import { PagetwoComponent } from './components/common/pagetwo/pagetwo.component';
+import { PagethreeComponent } from './components/common/pagethree/pagethree.component';
+import { PagefourComponent } from './components/common/pagefour/pagefour.component';
+import { PagefiveComponent } from './components/common/pagefive/pagefive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainPageComponent,
+    PageoneComponent,
+    PagetwoComponent,
+    PagethreeComponent,
+    PagefourComponent,
+    PagefiveComponent
   ],
   imports: [
     BrowserModule,
